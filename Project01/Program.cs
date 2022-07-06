de081a5de569a36529cbd29f2e9b878b2b2d82f3
RAVI <RAVI@RAVIS>
2022-07-06 15:19:20 +0530

    Add project files.

---
+++ Project01/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+namespace Project01
+{ 
+    class Program
+    {
+    
+      static void Main(string[] args)
+      {
+        Console.WriteLine("Hello, World!");
+      }
+    }
+}
---
+++ Project01/ProgramBase.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Project01
+{
+    internal class ProgramBase
+    {
+    }
+}
